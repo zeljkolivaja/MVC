09b467d211569d774cc883bd228a196d168e99ad
zeljkolivaja <zeljaos@gmail.com>
2023-11-20 17:07:39 +0100

    upgraded ErrorController with headers, added error loging

--- app/controllers/ErrorController.php
+++ app/controllers/ErrorController.php
@@ -2,23 +2,35 @@
 
 class ErrorController extends Controller
 {
-
     public function __construct()
     {
         parent::__construct();
-
     }
 
     public function pageNotFound($message)
     {
-        $this->view->render('error/pageNotFound', ["message" => $message]);
-        die();
+        $this->logError("404 - Page Not Found: $message");
+        $this->renderError('error/pageNotFound', 404, ["message" => $message]);
+        exit();
     }
 
     public function forbidden()
     {
-        $this->view->render('error/forbidden');
-        die();
+        $this->logError("403 - Forbidden");
+        $this->renderError('error/forbidden', 403);
+        exit();
     }
 
+    private function renderError($view, $statusCode, $data = [])
+    {
+        http_response_code($statusCode);
+        $this->view->render($view, $data);
+    }
+
+    private function logError($message)
+    {
+        $logFile = ROOT . DS . 'var' . DS . 'errors' . DS  . 'error.log';
+        $data = date('Y-m-d H:i:s') . ' - ' . $message;
+        error_log($data . PHP_EOL, 3, $logFile);
+    }
 }
